e988b642a8e565b2c8462908582d1be634909132
Valerii Tikhomirov <v.tikhomirov.dev@gmail.com>
2024-01-29 23:15:53 +0300

    Parsel -> Vite

--- src/ym.js
+++ src/ym.js
@@ -1,29 +1,39 @@
-
+const YM_ID = import.meta.env.VITE_YM_ID;
 const options = {
-    id: process.env.YM_ID,
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true
+  id: YM_ID,
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
 };
-    (function (m, e, t, r, i, k, a) {
-        m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-        m[i].l = 1 * new Date();
-        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+(function (m, e, t, r, i, k, a) {
+  m[i] =
+    m[i] ||
+    function () {
+      (m[i].a = m[i].a || []).push(arguments);
+    };
+  m[i].l = 1 * new Date();
+  (k = e.createElement(t)),
+    (a = e.getElementsByTagName(t)[0]),
+    (k.async = 1),
+    (k.src = r),
+    a.parentNode.insertBefore(k, a);
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
 const YandexMetrika = {
-    reachGoal: (targetName = '', params = {}) => ym(options.id, 'reachGoal', targetName, params),
-    hit: (url = '', options = {}) => ym(options.id, 'hit', url, options),
-    addFileExtension: (extensions) => ym(options.id, 'addFileExtension', extensions),
-    extLink: (url = '', options = {}) => ym(options.id, 'extLink', url, options),
-    file: (url = '', options = {}) => ym(options.id, 'file', url, options),
-    getClientID: (callback = (clientID) => clientID) => ym(options.id, 'getClientID', callback),
-    notBounce: (options = {}) => ym(options.id, 'notBounce', options = {}),
-    params: (params = {}) => ym(options.id, 'params', params),
-    replacePhones: () => ym(options.id, 'replacePhones'),
-    setUserID: (userID) => ym(options.id, 'setUserID', userID),
-    userParams: (params = {}) => ym(options.id, 'userParams', params),
+  reachGoal: (targetName = "", params = {}) =>
+    ym(options.id, "reachGoal", targetName, params),
+  hit: (url = "", options = {}) => ym(options.id, "hit", url, options),
+  addFileExtension: (extensions) =>
+    ym(options.id, "addFileExtension", extensions),
+  extLink: (url = "", options = {}) => ym(options.id, "extLink", url, options),
+  file: (url = "", options = {}) => ym(options.id, "file", url, options),
+  getClientID: (callback = (clientID) => clientID) =>
+    ym(options.id, "getClientID", callback),
+  notBounce: (options = {}) => ym(options.id, "notBounce", (options = {})),
+  params: (params = {}) => ym(options.id, "params", params),
+  replacePhones: () => ym(options.id, "replacePhones"),
+  setUserID: (userID) => ym(options.id, "setUserID", userID),
+  userParams: (params = {}) => ym(options.id, "userParams", params),
 };
 
 ym(options.id, "init", options);
